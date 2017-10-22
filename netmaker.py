28e445afad84c5fb1b94cb2122849054b7962fee
Nenad Cetic <nenad.cetic@gmail.com>
2017-10-22 11:37:20 +0200

    Core reorganization and various bugfixing.
    Introduces main() routines in two modules so modules can be imported without executing script code
    CV2 is only officialy available with 2.7 Puthon - removed prtint() Python3 where used.
    Various style and format fixes
    Fixed bugs with wrong names: "nomd", "sum"
    Cannot use sum as variable name. It is a keyword
    Other dead code removal.
    
    TODO: netmaker.py needs more work. This commit is mostly code reorganization.

--- netmaker.py
+++ netmaker.py
@@ -1,50 +1,55 @@
 import cv2
-import numpy
+import numpy as np
 
+def readImage(imageFileName):
+    img = cv2.imread(imageFileName)
+    if(img is None):
+        print("File " + imageFileName + " not found.")
+    return img 
 
+def buildnet(img):
+    # tag 2 is for start
+    # tag 5 is for end of the maze
+    # tag 1 is for wall
+    # tag 0 is for path
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-img = cv2.imread('test2green.jpg')
-net=numpy.zeros(shape=(15,15))
-img=cv2.resize(img,(150,150),interpolation = cv2.INTER_CUBIC) #
-   #should be  replaced with the rrasbberpy contoloing res on taking the picture
-   #this part is done, it controlsthe conversion picture->matrix
-def box_value(a,b):
-   green=0
-   sum=0
-   for i in range((img.shape[0]//15)*a,(img.shape[0]//15)*(a+1)):
-       for j in range((img.shape[1]//15)*b,(img.shape[1]//15)*(b+1)):
-          px=img[i,j]
-          sum=sum+px[0]+px[1]+px[2]
-          if (px[1]>200) and (px[0]<100) and (px[2]<100):
-              green=green+1    #green tile represents the end of the maze
+    lowGreen = np.array([40, 100, 100])
+    upGreen = np.array([80, 255, 255])
+    greenMask = cv2.inRange(hsv, lowGreen, upGreen)
 
-   if green > 50:
-        sum=1
+    lowRed = np.array([0, 50, 50])
+    upRed = np.array([50, 255, 255])
+    redMask = cv2.inRange(hsv, lowRed, upRed)
 
-   return sum
+    cv2.imshow('img', img)
+    cv2.imshow('greenMask', greenMask)
+    cv2.imshow('redMask', redMask)
 
+    net = np.zeros(shape=(15, 15))
+    # TODO: add logic here
+    for a in range(0, 15):
+        for b in range(0, 15):
+            net[a, b] = 2
 
-for i in range(0,15):
-    for j in range(0,15):
-        x=img.shape[0]//15*i
-        y=img.shape[1]//15*j
-        if box_value(i,j)==1:
-            net[i,j]=2
-        elif box_value(i,j)<30000:
-            net[i,j]=1
-        else:
-            net[i,j]=0
+    return net
 
+def printNet(net):
+    for i in range(0, 15):
+        for j in range(0, 15):
+            print (net[i, j] + " ")
+        print("\n")
 
+def main():
+    img = readImage("lavirinttesst.jpg")
+    if(img is None):
+        return
+    
+    net = buildnet(img)
+    print(net)
+    
+    cv2.waitKey(0)
 
-for i in range(0,15):
-    for j in range(0,15):
-        print (net[i,j], end=" ")
-    print()
+if __name__ == "__main__":
+    main()
 
-
-
-print()
-
-cv2.imshow("slika", img)
-cv2.waitKey(0)
