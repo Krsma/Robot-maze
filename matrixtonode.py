e6f46f0a1cf2e144bb4395733d87cfbcbb33db34
Krsma <vlada.krsmanovic@gmail.com>
2017-11-21 20:01:36 +0100

    another partial fix to the mazesolver issue

--- matrixtonode.py
+++ matrixtonode.py
@@ -1,5 +1,6 @@
 from netmaker import buildnet, readImage, Tag
 
+
 class Node(object):
     north = None
     south = None
@@ -26,7 +27,6 @@ class Node(object):
             elif net[self.positionx, i] == Tag.WALL:
                 break
 
-
         for i in range(1, self.positiony):  # edge is always 1 so we skip that one
             if net[self.positionx, i] == Tag.HAS_NODE:
                 for j in range(0, len(nodelist)):
@@ -36,7 +36,6 @@ class Node(object):
 
             elif net[self.positionx, i] == Tag.WALL:
                 break
-
 
         for j in range(self.positionx, 15):
             if net[j, self.positiony] == Tag.HAS_NODE:
@@ -58,60 +57,64 @@ class Node(object):
             elif net[j, self.positiony] == Tag.WALL:
                 break
 
+
 def solvethemaze(path):
     if len(path) == 0:
         print("List is empty")
         return
     current = path[- 1]  # take last node
 
+    if current.tag == 5:
+        return (path)
 
-    if not(current.east == None):
+    if not (current.east == None):
         if (current.east.tag == Tag.FINAL):
             print("found the end")
             path.append(current.east)
-            return(path)
+            return (path)
 
         elif ((current.east.tag == Tag.PATH) and (current.east.beenhere == False)):
             current.beenhere = True
             path.append(current.east)
-            return(solvethemaze(path))
+            return (solvethemaze(path))
 
-    if not(current.west == None):
+    if not (current.west == None):
         if (current.west.tag == Tag.FINAL):
             print("found the end")
             path.append(current.west)
-            return(path)
+            return (path)
 
         elif ((current.west.tag == Tag.PATH) and (current.west.beenhere == False)):
             current.beenhere = True
             path.append(current.west)
-            return(solvethemaze(path))
+            return (solvethemaze(path))
 
-    if not(current.north == None):
+    if not (current.north == None):
         if (current.north.tag == Tag.FINAL):
             path.append(current.north)
             print("found the end")
-            return(path)
+            return (path)
 
-        elif((current.north.tag == Tag.PATH) and (current.north.beenhere == False)):
+        elif ((current.north.tag == Tag.PATH) and (current.north.beenhere == False)):
             current.beenhere = True
             path.append(current.north)
-            return(solvethemaze(path))
+            return (solvethemaze(path))
 
-    if not(current.south == None):
+    if not (current.south == None):
         if (current.south.tag == Tag.FINAL):
             path.append(current.south)
             print("found the end")
-            return(path)
+            return (path)
 
-        elif((current.south.tag == Tag.PATH) and (current.south.beenhere == False)):
+        elif ((current.south.tag == Tag.PATH) and (current.south.beenhere == False)):
             current.beenhere = True
             path.append(current.south)
-            return(solvethemaze(path))
+            return (solvethemaze(path))
+
 
 def main():
     img = readImage("lavirinttesst.jpg")
-    if(img is None):
+    if (img is None):
         return
 
     nodelist = []
@@ -121,33 +124,26 @@ def main():
 
     for k in range(0, 14):  # should be changed to 0..14 and the 15 line is used as an edge
         for f in range(0, 14):
-            if not(net[k, f] == Tag.WALL):
-                a = (net[k, f + 1] == Tag.WALL)
-                b = (net[k, f - 1] == Tag.WALL)
-                c = (net[k + 1, f] == Tag.WALL)
-                d = (net[k - 1, f] == Tag.WALL)
-                if (((a and not b) or (b and not a)) or ((c and not d) or (d and not c))) or (net[k, f] == Tag.FINAL) or (net[k, f] == Tag.START):
-                    # i slightly changed this algorithm
-                    # https://youtu.be/rop0W4QDOUI?t=7m56s
-                    if net[k, f] == Tag.FINAL:
-                        x = Node(k, f, Tag.FINAL)
-                    elif net[k, f] == Tag.START:
-                        x = Node(k, f, Tag.START)
-                    else:
-                        x = Node(k, f, Tag.WALL)  # add tag giving into init
+            if not (net[k, f] == Tag.WALL):
+                if net[k, f] == Tag.FINAL:
+                      x = Node(k, f, Tag.FINAL)
+                elif net[k, f] == Tag.START:
+                      x = Node(k, f, Tag.START)
+                else:
+                      x = Node(k, f, Tag.WALL)  # add tag giving into init
 
-                    net[k, f] = Tag.HAS_NODE
-                    nodelist.append(x)
-
+                net[k, f] = Tag.HAS_NODE
+                nodelist.append(x)
 
     for j in range(0, len(nodelist)):  # go through all nodes you found and connect them
-            nodelist[j].connect(net, nodelist)
-            if nodelist[j].tag == Tag.START:
-                path.append(nodelist[j])
-                 #print("have beginning of the list")
+        nodelist[j].connect(net, nodelist)
+        if nodelist[j].tag == Tag.START:
+            path.append(nodelist[j])
+            # print("have beginning of the list")
 
     path = solvethemaze(path)
     print(path)
+
 
 if __name__ == "__main__":
     main()
